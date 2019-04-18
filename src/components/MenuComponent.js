849d12258573f3083009fc2977010567eba97849
Veno515 <veinoyusuf@gmail.com>
2019-04-18 10:13:32 +0100

    Components Part 2

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,67 +1,49 @@
 import React, { Component } from 'react';
-import { Media } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class Menu extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      selectedDish: null
+    }
+  }
 
-      this.state = {
-        dishes: [
-          {
-            id: 0,
-            name:'Pizza',
-            image:'assets/images/pizza.png',
-            category:'mains',
-            label:'Hot',
-            price:'15.99',
-            description:'A unique pizza for you.'
-          },
-          {
-            id: 1,
-            name:'Ice Cream',
-            image:'assets/images/cream.jpg',
-            category:'dessert',
-            label:'',
-            price:'4.99',
-            description:'One of the best.'
-          },
-          {
-            id: 2,
-            name:'Chips',
-            image:'assets/images/p-chips.jpg',
-            category:'snack',
-            label:'',
-            price:'10.99',
-            description:'Crispy potatoe chips.'
-          },
-          {
-            id: 3,
-            name:'Burger',
-            image:'assets/images/burger.jpg',
-            category:'mains',
-            label:'Hot',
-            price:'13.99',
-            description:'Simply the best.'
-          }
-        ]
-      }
+  onDishSelect(dish) {
+    this.setState({ selectedDish: dish});
+  }
+
+  renderDish(dish) {
+    if (dish != null) {
+        return (
+          <Card>
+            <CardImg width ="100%" src={dish.image} alt={dish.name} />
+            <CardBody>
+              <CardTitle>{dish.name}</CardTitle>
+              <CardText>{dish.description}</CardText>
+            </CardBody>
+          </Card>
+        );
+    }
+    else {
+      return(
+        <div></div>
+      )
+    }
   }
 
   render() {
 
-    const menu = this.state.dishes.map((dish) => {
+    const menu = this.props.dishes.map((dish) => {
       return (
-        <div key={dish.id} className="col-12 mt-5">
-          <Media tag="li">
-            <Media left middle>
-              <Media object src={dish.image} alt={dish.name} />
-            </Media>
-            <Media body className="ml-5">
-              <Media heading>{dish.name}</Media>
-              <p>{dish.description}</p>
-            </Media>
-          </Media>
+        <div key={dish.id} className="col-12 col-md-5 m-1">
+          <Card onClick ={() => this.onDishSelect(dish)}>
+              <CardImg width ="100%" src={dish.image} alt={dish.name} />
+            <CardImgOverlay>
+              <CardTitle>{dish.name}</CardTitle>
+            </CardImgOverlay>
+          </Card>
         </div>
       );
     });
@@ -69,9 +51,10 @@ class Menu extends Component {
     return (
       <div className="container">
         <div className="row">
-          <Media list>
             {menu}
-          </Media>
+        </div>
+        <div className="row">
+            {this.renderDish(this.state.selectedDish)}
         </div>
       </div>
     );
---
+++ src/shared/dishes.js
@@ -0,0 +1,187 @@
+export const DISHES =
+ [
+  {
+    id: 0,
+    name:'Pizza',
+    image:'assets/images/pizza.png',
+    category:'mains',
+    label:'Hot',
+    price:'15.99',
+    description:'A unique pizza for you.',
+    comments: [
+      {
+        id: 0,
+        ratings: 5,
+        comments: "Imagine all the eatables.",
+        author: "John Lemon",
+        date: "2019-10-16T17:57:28.556094Z"
+      },
+      {
+        id: 1,
+        ratings: 5,
+        comments: "Just eat it!",
+        author: "John Doe",
+        date: "2017-10-16T17:57:28.556094Z"
+      },
+      {
+        id: 2,
+        ratings: 4,
+        comments: "You are the best!.",
+        author: "Jane Doe",
+        date: "2019-12-13T17:57:28.556094Z"
+      },
+      {
+        id: 3,
+        ratings: 5,
+        comments: "Reaching for the stars!.",
+        author: "McVities",
+        date: "2011-01-04T17:57:28.556094Z"
+      },
+      {
+        id: 4,
+        ratings: 2,
+        comments: "Improve guys.",
+        author: "Stewie",
+        date: "2010-10-16T17:57:28.556094Z"
+      }
+    ]
+  },
+  {
+    id: 1,
+    name:'Ice Cream',
+    image:'assets/images/cream.jpg',
+    category:'dessert',
+    label:'',
+    price:'4.99',
+    description:'One of the best.',
+    comments: [
+      {
+        id: 0,
+        ratings: 5,
+        comments: "I couldn't resist.",
+        author: "John Lemon",
+        date: "2019-10-16T17:57:28.556094Z"
+      },
+      {
+        id: 1,
+        ratings: 5,
+        comments: "Refreshed!",
+        author: "John Doe",
+        date: "2017-10-16T17:57:28.556094Z"
+      },
+      {
+        id: 2,
+        ratings: 4,
+        comments: "Sent from heaven!",
+        author: "Jane Doe",
+        date: "2019-12-13T17:57:28.556094Z"
+      },
+      {
+        id: 3,
+        ratings: 5,
+        comments: "Wow!",
+        author: "McVities",
+        date: "2011-01-04T17:57:28.556094Z"
+      },
+      {
+        id: 4,
+        ratings: 2,
+        comments: "I want more.",
+        author: "Stewie",
+        date: "2010-10-16T17:57:28.556094Z"
+      }
+    ]
+  },
+  {
+    id: 2,
+    name:'Chips',
+    image:'assets/images/p-chips.jpg',
+    category:'snack',
+    label:'',
+    price:'10.99',
+    description:'Crispy potatoe chips.',
+    comments: [
+      {
+        id: 0,
+        ratings: 5,
+        comments: "The unimaginable.",
+        author: "John Lemon",
+        date: "2019-10-16T17:57:28.556094Z"
+      },
+      {
+        id: 1,
+        ratings: 5,
+        comments: "Just eat it!",
+        author: "John Doe",
+        date: "2017-10-16T17:57:28.556094Z"
+      },
+      {
+        id: 2,
+        ratings: 4,
+        comments: "How can I get more of this?",
+        author: "Jane Doe",
+        date: "2019-12-13T17:57:28.556094Z"
+      },
+      {
+        id: 3,
+        ratings: 5,
+        comments: "Extraordinary!",
+        author: "McVities",
+        date: "2011-01-04T17:57:28.556094Z"
+      },
+      {
+        id: 4,
+        ratings: 2,
+        comments: "I love this!",
+        author: "Stewie",
+        date: "2010-10-16T17:57:28.556094Z"
+      }
+    ]
+  },
+  {
+    id: 3,
+    name:'Burger',
+    image:'assets/images/burger.jpg',
+    category:'mains',
+    label:'Hot',
+    price:'13.99',
+    description:'Simply the best.',
+    comments: [
+      {
+        id: 0,
+        ratings: 5,
+        comments: "Imagine all the eatables.",
+        author: "John Lemon",
+        date: "2019-10-16T17:57:28.556094Z"
+      },
+      {
+        id: 1,
+        ratings: 5,
+        comments: "Just eat it!",
+        author: "John Doe",
+        date: "2017-10-16T17:57:28.556094Z"
+      },
+      {
+        id: 2,
+        ratings: 4,
+        comments: "You are the best!.",
+        author: "Jane Doe",
+        date: "2019-12-13T17:57:28.556094Z"
+      },
+      {
+        id: 3,
+        ratings: 5,
+        comments: "Reaching for the stars!.",
+        author: "McVities",
+        date: "2011-01-04T17:57:28.556094Z"
+      },
+      {
+        id: 4,
+        ratings: 2,
+        comments: "Improve guys.",
+        author: "Stewie",
+        date: "2010-10-16T17:57:28.556094Z"
+      }
+    ]
+  }
+]
